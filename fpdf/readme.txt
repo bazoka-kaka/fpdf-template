f3ab1b5eb31978d92ab20a8f86db467a03ee4165
bazoka-kaka <yehezkielwiradhika@gmail.com>
2023-11-29 10:53:55 +0700

    add fpdf kyb template

---
+++ fpdf/readme.txt
@@ -0,0 +1,36 @@
+FBI Agent Clothes 1.0
+https://www.saintsrowmods.com/forum/members/gumshoe.311499/
+
+-----------------------------
+RELEASE NOTES :
+
+1.0 	(Sep 08, 2020) Three edited clothes items:
+	- Urban Safari With Collared Shirt (FBI Jacket)
+	- Shoot 'em Up Tee (FBI T-Shirt)
+	- Steampunk Fighter Pants (FBI Badge + Holster)
+
+-----------------------------
+INSTALL INSTRUCTIONS :
+
+1. 	Extract this archive.
+2. 	Copy all files (except readme.txt) to Saints Row The Third install folder
+	...\steam\steamapps\common\saints row the third\
+3.	Launch Saints Row The Third and have fun =D
+
+-----------------------------
+IMPORTANT NOTE :
+
+-	If you use other custom character/clothes mods, you will need to merge the customize_item.asm_pc of this mod with your existing one.
+-	See here: https://www.saintsrowmods.com/forum/threads/sr3-asm_pc-files-updater.1561/
+-	Your game will crash if you don't do this!
+
+
+-----------------------------
+CREDITS:
+
+Kaivai - Texturing guide on how to make this
+ViperVenom - Guides/tutorials
+IdolNinja - Documentation and general awesomeness
+Volition - Original base assets
+Gumshoe - Textures and putting this all together
+
---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+
+require_once __DIR__ . '/fpdf/fpdf.php';
+
+class PDF extends FPDF
+{
+  // Load data
+  function LoadData($file)
+  {
+    // Read file lines
+    $lines = file($file);
+    $data = array();
+    foreach ($lines as $line)
+      $data[] = explode(';', trim($line));
+    return $data;
+  }
+
+  // Better table
+  function ImprovedTable($header, $data)
+  {
+    // Column widths
+    $w = array(40, 35, 40, 45);
+    // Header
+    for ($i = 0; $i < count($header); $i++)
+      $this->Cell($w[$i], 7, $header[$i], 1, 0, 'C');
+    $this->Ln();
+    // Data
+    foreach ($data as $row) {
+      $this->Cell($w[0], 6, $row[0], 'LR');
+      $this->Cell($w[1], 6, $row[1], 'LR');
+      $this->Cell($w[2], 6, number_format($row[2]), 'LR', 0, 'R');
+      $this->Cell($w[3], 6, number_format($row[3]), 'LR', 0, 'R');
+      $this->Ln();
+    }
+    // Closing line
+    $this->Cell(array_sum($w), 0, '', 'T');
+  }
+
+  // Page footer
+  function Footer()
+  {
+    // Position at 1.5 cm from bottom
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial', 'I', 8);
+    // Page number
+    $this->Cell(0, 10, 'Page ' . $this->PageNo(), 0, 0, 'C');
+  }
+}
+
+$pdf = new PDF('L');
+// Column headings
+$header = array('Country', 'Capital', 'Area (sq km)', 'Pop. (thousands)');
+// Data loading
+$data = $pdf->LoadData(__DIR__ . '/fpdf/tutorial/countries.txt');
+$pdf->AddPage();
+// Heading Main Title
+$pdf->Image('logo.png', 11, 11, 30);
+$pdf->Cell(32);
+$pdf->SetFont('Arial', 'B', 18);
+$pdf->Cell(0, 10, 'Training Center');
+$pdf->Cell(0, 10, 'Statistics Report', 0, 0, 'R');
+$pdf->Ln();
+$pdf->SetFont('Arial', '', 12);
+$pdf->Cell(0, 3, 'Printed in ' . date("Y-m-d") . ' at ' . date("H:i:s") . ' by Yehezkiel Wiradhika');
+$pdf->Ln(7);
+// Add Filters
+$pdf->SetFont('Arial', '', 14);
+$pdf->Cell(0, 5, "Department: Management Information System");
+$pdf->Ln(7);
+$pdf->Cell(0, 5, "Section: Application");
+$pdf->Ln(7);
+$pdf->Cell(0, 5, "Subsection: Programming");
+$pdf->Ln(10);
+// Add Table
+$pdf->SetFont('Arial', '', 14);
+$pdf->ImprovedTable($header, $data);
+$pdf->Output();
